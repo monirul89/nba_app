ee070953cbb4dcb942a2c8072af17c2c29cfc7bd
monirul89 <monirul89@gmail.com>
2018-11-26 23:51:48 +0600

    nba

---
+++ precache-manifest.3b66927a00cd85638aef42c85daf812c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "94c705b17b4a75b66266",
+    "url": "/static/js/main.94c705b1.chunk.js"
+  },
+  {
+    "revision": "003e4148251ae0e8e80b",
+    "url": "/static/js/1.003e4148.chunk.js"
+  },
+  {
+    "revision": "94c705b17b4a75b66266",
+    "url": "/static/css/main.ea5a5e7a.chunk.css"
+  },
+  {
+    "revision": "0765c13936e2c4be0781cb57d8edd528",
+    "url": "/index.html"
+  }
+];
